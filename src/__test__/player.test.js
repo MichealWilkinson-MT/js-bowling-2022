1215d479582f0d8235de8da4f5112c77a2e829b4
MichealWilkinson-MT <88385899+MichealWilkinson-MT@users.noreply.github.com>
2022-03-02 10:57:09 +0000

    Added skeleton files

---
+++ src/__test__/player.test.js
@@ -0,0 +1,9 @@
+const {
+    BowingPlayer
+} = require('../player');
+
+describe('Get player points', () => {
+    it('Given a player has rolled no balls their score should be 0', () => {
+        // The test for this needs to go here
+    });
+});
---
+++ src/player.js
@@ -0,0 +1,7 @@
+class BowingPlayer {
+
+}
+
+module.exports = {
+    BowingPlayer
+}
