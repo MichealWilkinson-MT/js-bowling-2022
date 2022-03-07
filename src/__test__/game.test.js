493eb92a07b7e9aa23d1ffd6187e0b608273aa96
AJ Lobb <ajlobb@live.com>
2022-03-07 15:16:41 +0000

    created code and test for the bowling players

--- src/__test__/game.test.js
+++ src/__test__/game.test.js
@@ -4,9 +4,8 @@ const {
 
 
 describe('check game has players', () => {
-    xit('check the game has at least 1 player', () => {
-        const game = new BowlingGame();
-        game.
-        expect().toEqual(0,10);
+    it('check the game has at least 1 player', () => {
+        const game = new BowlingGame(["Tom", "AJ", "Jamie", "Callum","Mark"]);
+        expect(game.players.length).toEqual(5);
     })
 })
--- src/game.js
+++ src/game.js
@@ -1,7 +1,14 @@
+const { BowlingPlayer } = require("./player")
 
 
 class BowlingGame {
-
+    constructor(players) {
+        this.players = []
+        for (let i = 0; i < players.length; i++){
+            const playerName = players[i]
+            this.players.push(new BowlingPlayer(playerName))
+        }
+}
 
 }
 
