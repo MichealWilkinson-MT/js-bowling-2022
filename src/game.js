9be41c56afb2debccc57863697cfb18b735b85c8
AJ Lobb <ajlobb@live.com>
2022-03-08 15:09:34 +0000

    added comments

--- src/game.js
+++ src/game.js
@@ -1,19 +1,31 @@
 const { BowlingPlayer } = require("./player")
 
-
 class BowlingGame {
     currentPlayerID = 0
     framenumber = 1
+    /**
+     * constructor
+     * @param {string[]} players 
+     */
     constructor(players) {
         this.players = []
         for (let i = 0; i < players.length; i++){
             const playerName = players[i]
             this.players.push(new BowlingPlayer(playerName))
         }
-}
+    }
+    /**
+     * checks the current player turn
+     * @returns {BowlingPlayer}
+     */
     currentPlayerTurn = () => {
         return this.players[this.currentPlayerID]
     }
+    /**
+     * if the players have all had a go it resets to the first players turn
+     * @param {number} score 
+     * @returns {number}
+     */
     recordScore = (score) => {
 
          const isValid = this.players[this.currentPlayerID].recordBall(score)
@@ -28,6 +40,10 @@ class BowlingGame {
 
         return isValid
     }
+    /**
+     * Function to see if the game is over
+     * @returns {boolean}
+     */
     isGameFinished = () => {
         if (this.players[this.currentPlayerID].isFrameComplete(this.framenumber < 11 )) {
             return true 
